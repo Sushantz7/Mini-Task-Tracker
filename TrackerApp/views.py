dece2a7412a4bb77980f15f0c9532eb1f33d6486
Sushantz7 <sushantwork777@gmail.com>
2025-09-19 12:40:21 +0545

    feat: create view for register page.

--- TrackerApp/views.py
+++ TrackerApp/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.views import LoginView
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .models import CustomUser
 # Create your views here.
 
 
@@ -12,3 +14,10 @@ class LoginPageView(LoginView):
     template_name = "registration/login.html"
     authentication_form = LoginForm
     redirect_authenticated_user = True  # To redirect the authenticated user to the LOGIN_REDIRECT_URL defined in the settings.
+
+
+class RegisterPageView(CreateView):
+    template_name = "registration/register.html"
+    form_class = RegistrationForm
+    model = CustomUser
+    success_url = reverse_lazy("login")
