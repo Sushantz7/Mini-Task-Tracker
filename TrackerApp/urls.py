c47be3a909361cbbbbe1a94d2e7f750723dbf5a8
Sushantz7 <sushantwork777@gmail.com>
2025-09-19 11:42:51 +0545

    feat: add login and logout urls.

--- TrackerApp/urls.py
+++ TrackerApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TrackerHomeView
+from .views import TrackerHomeView, LoginPageView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", TrackerHomeView.as_view(), name="home"),
+    path("login/", LoginPageView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
