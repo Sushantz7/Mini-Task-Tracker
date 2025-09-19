e669a3cdf5fc8d0cc6b8c6e3b55cd99ee2c449de
Sushantz7 <sushantwork777@gmail.com>
2025-09-19 11:43:21 +0545

    feat: create view for login page.

--- TrackerApp/views.py
+++ TrackerApp/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from .forms import LoginForm
 # Create your views here.
+
 
 class TrackerHomeView(TemplateView):
     template_name = "LandingPage.html"
+
+
+class LoginPageView(LoginView):
+    template_name = "registration/login.html"
+    authentication_form = LoginForm
+    redirect_authenticated_user = True  # To redirect the authenticated user to the LOGIN_REDIRECT_URL defined in the settings.
