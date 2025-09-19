1d7b3f568e75dfec180603c4d153c5962b04e7fb
Sushantz7 <sushantwork777@gmail.com>
2025-09-19 11:40:16 +0545

    feat: add customuser admin.

--- TrackerApp/admin.py
+++ TrackerApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    pass
+admin.site.register(CustomUser,CustomUserAdmin)
