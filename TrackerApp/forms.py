051d8c886944871fc3253aec62808c0efbb3fa1d
Sushantz7 <sushantwork777@gmail.com>
2025-09-22 11:13:10 +0545

    feat: add form for Task creation and deletion.

--- TrackerApp/forms.py
+++ TrackerApp/forms.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
-from .models import CustomUser
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import CustomUser, Task
 
 
 class LoginForm(AuthenticationForm):
@@ -56,3 +57,36 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("email", "password1", "password2")
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = [
+            "task_name",
+            "start_date",
+            "est_end_date",
+            "category",
+            "numeric_target",
+            "numeric_current",
+            "status",
+            "priority",
+            "notes",
+        ]
+        widgets = {
+            "task_name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Task Name"}
+            ),
+            "start_date": forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}
+            ),
+            "est_end_date": forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}
+            ),
+            "numeric_target": forms.NumberInput(attrs={"class": "form-control"}),
+            "numeric_current": forms.NumberInput(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-select"}),
+            "status": forms.Select(attrs={"class": "form-select"}),
+            "priority": forms.Select(attrs={"class": "form-select"}),
+            "notes": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+        }
